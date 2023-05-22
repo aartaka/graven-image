d91cd50f52ac15e8313391c7da1748cf0e39c144
Artyom Bologov <aartaka@protonmail.com>
2023-05-22 20:30:40 +0400

    time: Return depending on RETURN-KIND portably.

--- source/time.lisp
+++ source/time.lisp
@@ -45,41 +45,45 @@ The way information is returned depends on RETURN-KIND:
   - (:aborted . boolean)."
   (let ((props (gensym "PROPS")))
     `(let ((,props (list)))
-       #+sbcl
-       (sb-ext::call-with-timing
-        (lambda (&key real-time-ms user-run-time-us system-run-time-us
-                   gc-run-time-ms processor-cycles eval-calls
-                   lambdas-converted (page-faults 0) bytes-consed
-                   aborted)
-          (declare (ignorable processor-cycles eval-calls lambdas-converted))
-          (push (cons :aborted aborted) ,props)
-          (when real-time-ms
-            (push (cons :real (/ real-time-ms 1000)) ,props))
-          (when user-run-time-us
-            (push (cons :user (/ user-run-time-us 1000000)) ,props))
-          (when system-run-time-us
-            (push (cons :system (/ system-run-time-us 1000000)) ,props))
-          (when gc-run-time-ms
-            (push (cons :gc (/ gc-run-time-ms 1000)) ,props))
-          (when bytes-consed
-            (push (cons :allocated bytes-consed) ,props))
-          (push (cons :faults page-faults) ,props))
-        (lambda () ,form))
-       #+ccl
-       (let ((ccl::*report-time-function*
-               (lambda (&key form results elapsed-time user-time
-                          system-time gc-time bytes-allocated
-                          minor-page-faults major-page-faults
-                          swaps)
-                 (declare (ignorable swaps form))
-                 (let ((page-faults (+ minor-page-faults major-page-faults)))
-                   (unless (zerop page-faults)
-                     (push (cons :faults page-faults) ,props)))
-                 (push (cons :allocated bytes-allocated) ,props)
-                 (push (cons :real (/ elapsed-time internal-time-units-per-second)) ,props)
-                 (push (cons :system (/ system-time internal-time-units-per-second)) ,props)
-                 (push (cons :user (/ user-time internal-time-units-per-second)) ,props)
-                 (push (cons :gc (/ gc-time internal-time-units-per-second)) ,props)
-                 (push (cons :count (ccl::cpu-count)) ,props)(ccl::cpu-count)
-                 (values-list (append results (list (return-time-props ,return-kind ,props ',form)))))))
-         (ccl::report-time ',form (lambda () ,form))))))
+       (values-list
+        (append
+         (multiple-value-list
+          #+sbcl
+          (sb-ext::call-with-timing
+           (lambda (&key real-time-ms user-run-time-us system-run-time-us
+                      gc-run-time-ms processor-cycles eval-calls
+                      lambdas-converted (page-faults 0) bytes-consed
+                      aborted)
+             (declare (ignorable processor-cycles eval-calls lambdas-converted))
+             (push (cons :aborted aborted) ,props)
+             (when real-time-ms
+               (push (cons :real (/ real-time-ms 1000)) ,props))
+             (when user-run-time-us
+               (push (cons :user (/ user-run-time-us 1000000)) ,props))
+             (when system-run-time-us
+               (push (cons :system (/ system-run-time-us 1000000)) ,props))
+             (when gc-run-time-ms
+               (push (cons :gc (/ gc-run-time-ms 1000)) ,props))
+             (when bytes-consed
+               (push (cons :allocated bytes-consed) ,props))
+             (push (cons :faults page-faults) ,props))
+           (lambda () ,form))
+          #+ccl
+          (let ((ccl::*report-time-function*
+                  (lambda (&key form results elapsed-time user-time
+                             system-time gc-time bytes-allocated
+                             minor-page-faults major-page-faults
+                             swaps)
+                    (declare (ignorable swaps form))
+                    (let ((page-faults (+ minor-page-faults major-page-faults)))
+                      (unless (zerop page-faults)
+                        (push (cons :faults page-faults) ,props)))
+                    (push (cons :allocated bytes-allocated) ,props)
+                    (push (cons :real (/ elapsed-time internal-time-units-per-second)) ,props)
+                    (push (cons :system (/ system-time internal-time-units-per-second)) ,props)
+                    (push (cons :user (/ user-time internal-time-units-per-second)) ,props)
+                    (push (cons :gc (/ gc-time internal-time-units-per-second)) ,props)
+                    (push (cons :count (ccl::cpu-count)) ,props)(ccl::cpu-count)
+                    (values-list results))))
+            (ccl::report-time ',form (lambda () ,form))))
+         (list (return-time-props ,return-kind ,props ',form)))))))
