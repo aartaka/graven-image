24534c24a2fc2d07700b83574db474124a7a269f
Artyom Bologov <aartaka@protonmail.com>
2023-07-30 17:41:04 +0400

    package: Update and add gimage-user package.

--- source/package.lisp
+++ source/package.lisp
@@ -3,13 +3,14 @@
 
 (uiop:define-package :graven-image
   (:use :common-lisp)
+  (:nicknames :gimage)
   (:export
    #:y-or-n-p* #:yes-or-no-p* #:*yes-or-no-options*
    #:apropos-list* #:apropos* #:apropod*
    ;; #:ed*
-   #:describe* #:fields*
+   #:fields* #:description*
    #:$ #:definterface
-   #:inspect* #:description*
+   #:describe* #:inspect*
    #:function-lambda-expression* #:function-lambda-list* #:function-name* #:function-type*
    #:time* #:with-time*
    #:dribble*
@@ -24,4 +25,26 @@
    #+lispworks #:hcl
    #+allegro   #:excl
    #:package-local-nicknames)
-  (:documentation "CL standard debugging utilities improved."))
+  (:documentation "CL standard debugging utilities improved:
+- `y-or-n-p' -> `y-or-n-p*'.
+- `yes-or-no-p' -> `yes-or-no-p*'.
+  - See `*yes-or-no-options*' for `y-or-n-p*'/`yes-or-no-p*'
+    configuration.
+- `apropos-list' -> `apropos-list*'.
+- `apropos' -> `apropos*'.
+  - Also see `apropod*' for name+documentation search.
+- `describe' -> `describe*'.
+- `inspect' -> `inspect*'.
+  - Also see `fields*' and `description*' for the underlying
+    inspection utils.
+- `function-lambda-expression' -> `function-lambda-expression*'
+  - Also see `function-lambda-list*', `function-name*'
+    `function-type*'.
+- `time' -> `time*'.
+  - Also see `with-time*' with the exposed `time'-internal APIs.
+- `dribble' -> `dribble*'."))
+
+(uiop:define-package :graven-image-user
+  (:use :common-lisp :graven-image)
+  (:nicknames :gimage-user)
+  (:documentation "A user packages that's safe to use instead of CL-USER."))
